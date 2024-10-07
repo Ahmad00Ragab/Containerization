6194a4960c2d79e54d52f6db155e001f11d1a710
Ahmad Haroun <Ahmad.haroun2023@gmail.com>
2024-10-07 10:41:36 +0300

    add docker file

---
+++ Docker/src/test/java/com/example/AppTest.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.BeforeEach;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    // Optional: Use this if you need to set up before each test
+    @BeforeEach
+    public void setUp() {
+        // Initialization code can go here
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testApp() {
+        Assertions.assertTrue(true);
+    }
+}
